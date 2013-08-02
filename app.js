6e418b4bdbacbd5bfa2914493a89aeb9b8f8c752
kobylin <kobylin.a.a@gmail.com>
2013-08-02 11:47:05 +0300

    Refactor

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var canvas = document.getElementById('screen');
-console.log(canvas);
-var simulator = new Simulator({
-    canvas: canvas
+var canvas = document.getElementById('screen'),
+    viewport = new Viewport(canvas);
+
+var simulator = new Simulator();
+var walker = new Walker({
+    x: viewport.width / 2,
+    y: viewport.height / 2
 });
-simulator.addObject(new Walker({
-    x: canvas.width / 2,
-    y: canvas.height / 2
-}));
+var randomizer = new Randomizer();
+simulator.on('step', function(simulator, e) {
+    randomizer.update(e, viewport);
+    randomizer.draw(viewport);
+//    walker.update(e, viewport);
+//    walker.draw(viewport);
+});
 simulator.start();
