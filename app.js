2609cd5d7a0c34690cdecea5773d0fa59ef175be
kobylin <kobylin.a.a@gmail.com>
2013-08-09 17:05:49 +0300

    Finish Chapter 1

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 var canvas = document.getElementById('screen'),
-    viewport = new Viewport(canvas);
+    loggerOut = document.getElementById('logger'),
+    viewport = new Viewport(canvas, {
+        scale: 1
+    }),
+    logger = new Logger(loggerOut),
+    object;
 
-var simulator = new Simulator();
-var walker = new Walker({
-    x: viewport.width / 2,
-    y: viewport.height / 2
+simulator = new Simulator();
+//var object = new Walker({
+//    x: viewport.width / 2,
+//    y: viewport.height / 2
+//});
+//object = new Randomizer();
+//object = new Palette();
+//object = new BouncingBall({
+//    x: viewport.width / 2,
+//    y: viewport.height / 2,
+//    velocity: 100
+//});
+object = new Mover({
+    position: new Vector2(viewport.getWidth() / 2, viewport.getHeight() / 2),
+    acceleration: new Vector2(0.1, 0.2)
 });
-var randomizer = new Randomizer();
+
 simulator.on('step', function(simulator, e) {
-    randomizer.update(e, viewport);
-    randomizer.draw(viewport);
-//    walker.update(e, viewport);
-//    walker.draw(viewport);
+    object.update(e, viewport);
+    object.draw(e, viewport);
+    logger.print('X: ', Math.ceil(object.position.x), 'Y: ', Math.ceil(object.position.y));
 });
 simulator.start();
